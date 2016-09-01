072bc48436fa0b48bd4e1995fda6dfef52ce2284
thomasteal <tealbthomas@gmail.com>
2016-09-01 00:31:37 -0300

    first version of Bring2Me project in android studio ass thomas

---
+++ app/src/main/java/com/example/echo/bring2me/TelaCadastro.java
@@ -0,0 +1,13 @@
+package com.example.echo.bring2me;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro);
+    }
+}
