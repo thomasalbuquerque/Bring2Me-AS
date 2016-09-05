eb39fc7c0e5882f72047e1673804adae45939f5e
andreleinio <andreleinio@hotmail.com>
2016-09-05 00:06:29 -0300

    Registro/Cadastro/BDLocal

---
+++ app/src/main/java/com/example/echo/bring2me/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.echo.bring2me;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.101/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.101/android_login_api/register.php";
+}
