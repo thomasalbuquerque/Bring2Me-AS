b1694f8a1d02876f4849aff35779fe99b7a5dee9
tiagogodoi <tiago.nz@hotmail.com>
2016-09-15 00:21:30 -0300

    Pequenas mudanças no design da tela inicial

--- app/src/main/java/com/example/echo/bring2me/MainActivity.java
+++ app/src/main/java/com/example/echo/bring2me/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -17,6 +18,7 @@ public class MainActivity extends Activity {
     private TextView txtName;
     private TextView txtEmail;
     private Button btnLogout;
+    private TabHost tabHost;
 
     private SQLiteHandler db;
     private SessionManager session;
@@ -29,6 +31,23 @@ public class MainActivity extends Activity {
         txtName = (TextView) findViewById(R.id.name);
         txtEmail = (TextView) findViewById(R.id.email);
         btnLogout = (Button) findViewById(R.id.btnLogout);
+        tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabHost.TabSpec ts = tabHost.newTabSpec("tag1");
+        ts.setContent(R.id.Perfil);
+        ts.setIndicator("Perfil");
+        tabHost.addTab(ts);
+
+        ts = tabHost.newTabSpec("tag2");
+        ts.setContent(R.id.Iniciar);
+        ts.setIndicator("Iniciar");
+        tabHost.addTab(ts);
+
+        ts = tabHost.newTabSpec("tag3");
+        ts.setContent(R.id.Atividades);
+        ts.setIndicator("Atividades");
+        tabHost.addTab(ts);
 
         // SqLite database handler
         db = new SQLiteHandler(getApplicationContext());
