0ada1d139250b0f38e473161e672968f651219f3
andreleinio <andreleinio@hotmail.com>
2016-09-18 16:35:49 -0300

    mudanca no nome da classe CadastroViagem

--- app/src/main/java/com/example/echo/bring2me/CadastroViagem.java
+++ app/src/main/java/com/example/echo/bring2me/CadastroViagem.java
@@ -16,7 +16,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class CadastroViagens extends AppCompatActivity {
+public class CadastroViagem extends AppCompatActivity {
     private ArrayList paises = new ArrayList();
     private Spinner spPaisesOri;
     private Spinner spPaisesDes;
