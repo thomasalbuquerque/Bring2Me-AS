8a8728402f77d3af04d2015bb0d8857d34ceafdf
thomasteal <tealbthomas@gmail.com>
2016-09-17 22:10:28 -0300

    criados novos final pra url de imagem padrao q vai aparecer na listview e avaliacao padrão

--- app/src/main/java/com/example/echo/bring2me/AppConfig.java
+++ app/src/main/java/com/example/echo/bring2me/AppConfig.java
@@ -6,4 +6,8 @@ public class AppConfig {
 
     // Server user register url
     public static String URL_REGISTER = "http://echob2m.esy.es/bd_api/register.php";
+
+    public static String URL_IMAGEM = "https://lh4.googleusercontent.com/tmO3t6nytdpy1EG63SnGI2fy07rYXTUjaxQ2ONSmSsA1oHRav0Jbfo9gy-oPnnoXMIdFK6nYkXGsvV4=w1865-h941";
+
+    public static int AvaliacaoPadraoDoViajante = 5;    //por enquanto, não existe essa funcionalidade.
 }
