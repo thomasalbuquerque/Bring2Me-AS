4fae526978d13e6cd9b468975228c5273de4ff98
tiagogodoi <tiago.nz@hotmail.com>
2016-09-26 10:44:17 -0300

    Backbutton navigation na tela inicial

--- app/src/main/java/com/example/echo/bring2me/MainActivity.java
+++ app/src/main/java/com/example/echo/bring2me/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.echo.bring2me;
 import java.util.HashMap;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.app.TaskStackBuilder;
 
 public class MainActivity extends Activity {
 
@@ -98,10 +100,10 @@ public class MainActivity extends Activity {
 
             @Override
             public void onClick(View v) {
-                // Launching the login activity
+                //Construct the Intent you want to end up at
                 Intent intent = new Intent(MainActivity.this, CadastroViagem.class);
                 startActivity(intent);
-                finish();
+                //finish();
             }
         });
 
@@ -111,7 +113,7 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, BuscaViagensActivity.class);
                 startActivity(intent);
-                finish();
+                //finish();
             }
         });
     }
