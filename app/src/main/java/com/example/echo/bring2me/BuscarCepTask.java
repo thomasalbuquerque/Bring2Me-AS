ec6739577fdda6be52e93b01d39e57bc06e5e8c6
Felipe <Arquelau>
2016-10-27 13:16:31 -0200

    Novos campos adicionados ao pedido

---
+++ app/src/main/java/com/example/echo/bring2me/BuscarCepTask.java
@@ -0,0 +1,104 @@
+package com.example.echo.bring2me;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+import java.net.URL;
+
+public class BuscarCepTask extends AsyncTask<String, Void, String> {
+    URL url = null;
+    HttpURLConnection httpURLConnection = null;
+    private TextView bairroview;
+    private TextView logradouroview;
+    private TextView ufview;
+    private TextView localidadeview;
+
+    private String bairro;
+    private String logradouro;
+    private String uf;
+    private String localidade;
+
+
+    public BuscarCepTask(EditText bairroview, EditText logradouroview, EditText ufview, EditText localidadeview) {
+        this.bairroview = bairroview;
+        this.logradouroview = logradouroview;
+        this.ufview = ufview;
+        this.localidadeview = localidadeview;
+
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        StringBuilder result = null;
+        int respCode = -1;
+
+        try {
+            url = new URL("http://viacep.com.br/ws/" + params[0] + "/json/");
+            Log.d("buscacep", "Order Response: " + "http://viacep.com.br/ws/" + params[0] + "/json/");
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            do {
+                if (httpURLConnection != null) {
+                    respCode = httpURLConnection.getResponseCode();
+                }
+            } while (respCode == -1);
+
+            if (respCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+                result = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    result.append(line);
+                }
+                br.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (httpURLConnection != null) {
+                httpURLConnection.disconnect();
+                httpURLConnection = null;
+            }
+        }
+        Log.d("buscacep", "Order Response: " + result.toString());
+        return (result != null) ? result.toString() : null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        try {
+            JSONObject object = new JSONObject(s);
+            //object.
+            bairro = object.getString("bairro");
+            logradouro = object.getString("logradouro");
+            uf = object.getString("uf");
+            localidade = object.getString("localidade");
+            bairroview.setText(bairro);
+            logradouroview.setText(logradouro);
+            ufview.setText(uf);
+            localidadeview.setText(localidade);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/example/echo/bring2me/OrderActivity.java
+++ app/src/main/java/com/example/echo/bring2me/OrderActivity.java
@@ -9,6 +9,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request.Method;
@@ -33,7 +36,15 @@ public class OrderActivity extends Activity {
     private EditText inputValor;
     private EditText inputLink;
     private EditText inputProduct;
+    private EditText inputCep;
+    private EditText inputnumresid;
+    private EditText inputcomplemento;
+    private EditText bairroview;
+    private EditText logradouroview;
+    private EditText ufview;
+    private EditText localidadeview;
     private Button btnOrder;
+    private Button buscacep;
     private ProgressDialog pDialog;
     private SessionManager session;
     private SQLiteHandler db;
@@ -47,7 +58,15 @@ public class OrderActivity extends Activity {
         inputValor = (EditText) findViewById(R.id.valor);
         inputLink = (EditText) findViewById(R.id.link);
         inputProduct = (EditText) findViewById(R.id.produto);
+        inputCep = (EditText) findViewById(R.id.cep);
+        inputnumresid = (EditText) findViewById(R.id.numresidenciaedit);
+        inputcomplemento = (EditText) findViewById(R.id.complementoedit);
         btnOrder = (Button) findViewById(R.id.btnOrder);
+        buscacep = (Button) findViewById(R.id.buscarcep);
+        bairroview = (EditText) findViewById(R.id.bairroview);
+        logradouroview = (EditText) findViewById(R.id.logradouroview);
+        ufview = (EditText) findViewById(R.id.ufview);
+        localidadeview = (EditText) findViewById(R.id.localidadeview);
 
         // Progress dialog
         pDialog = new ProgressDialog(this);
@@ -67,21 +86,48 @@ public class OrderActivity extends Activity {
 
         final String email = user.get("email");
 
+        final RadioGroup radioGroup1 = (RadioGroup)findViewById(R.id.radioGroup);
+        final ToggleableRadioButton lC1 = (ToggleableRadioButton)findViewById(R.id.radioButton);
+        lC1.toggle();
+
+
+        buscacep.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String cep = inputCep.getText().toString().trim();
+                BuscarCepTask buscarCep = new BuscarCepTask(bairroview, logradouroview, ufview, localidadeview);
+                buscarCep.execute(cep);
+
+                }
+        });
+
+
         //Pegando o id da viagem do intent anterior
         Bundle extras = getIntent().getExtras();
         if(extras!=null) {
             id_viagem = extras.getString("id_viagem");
         }
 
-        // Order Button Click event
         btnOrder.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 String valor = inputValor.getText().toString().trim();
                 String link = inputLink.getText().toString().trim();
                 String product = inputProduct.getText().toString().trim();
-
-                if (!valor.isEmpty() && !product.isEmpty()) {
-                    Order( valor, link , product, email, id_viagem);
+                String cep = inputCep.getText().toString().trim();
+                String numresid = inputnumresid.getText().toString().trim();
+                String complemento= inputcomplemento.getText().toString().trim();
+                String caixa;
+                if(lC1.isChecked()){
+                    caixa = "true";
+                }
+                else caixa = "false";
+                String bairro = bairroview.getText().toString().trim();
+                String logradouro = bairroview.getText().toString().trim();
+                String uf = bairroview.getText().toString().trim();
+                String localidade = bairroview.getText().toString().trim();
+                if (!valor.isEmpty() && !product.isEmpty() && !cep.isEmpty() && !bairro.isEmpty() &&
+                        !logradouro.isEmpty() && !uf.isEmpty() && !localidade.isEmpty() && !numresid.isEmpty()) {
+                    Order( valor, link , product, email, id_viagem, caixa, cep,
+                            bairro, logradouro, uf, localidade, numresid, complemento);
                 } else {
                     Toast.makeText(getApplicationContext(),
                             "Não foi possível iniciar uma negociação. Verifique se os campos estão preenchidos!", Toast.LENGTH_LONG)
@@ -91,7 +137,9 @@ public class OrderActivity extends Activity {
         });
     }
 
-    private void Order(final String valor, final String link, final String product, final String email, final String id_viagem) {
+    private void Order(final String valor, final String link, final String product, final String email, final String id_viagem,
+                       final String caixa, final String cep, final String bairro, final String logradouro, final String uf,
+                       final String localidade, final String numresid, final String complemento) {
         // Tag used to cancel the request
         String tag_string_req = "req_order";
 
@@ -157,6 +205,14 @@ public class OrderActivity extends Activity {
                 params.put("product", product);
                 params.put("email", email);
                 params.put("id_viagem", id_viagem);
+                params.put("caixa", caixa);
+                params.put("cep", cep);
+                params.put("bairro", bairro);
+                params.put("logradouro", logradouro);
+                params.put("uf", uf);
+                params.put("localidade", localidade);
+                params.put("numresid", numresid);
+                params.put("complemento", complemento);
 
                 return params;
             }
