d503f07649f321582ea7dc70d7176cdb58194175
tiagogodoi <tiago.nz@hotmail.com>
2016-11-10 03:16:40 -0200

    Xmls de gerenciamento de pedidos

---
+++ app/src/main/java/com/example/echo/bring2me/ManageRequestsActivity.java
@@ -0,0 +1,38 @@
+package com.example.echo.bring2me;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ManageRequestsActivity extends AppCompatActivity {
+
+    Button btnFeitos, btnRecebidos;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_requests);
+
+        btnFeitos = (Button)findViewById(R.id.btnPedidosFeitos);
+        btnRecebidos = (Button)findViewById(R.id.btnPedidosRecebidos);
+
+        btnFeitos.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(ManageRequestsActivity.this, <nome_da_classe>.class);
+                //startActivity(intent);
+            }
+        });
+
+        btnRecebidos.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(ManageRequestsActivity.this, <nome_da_classe>.class);
+                //startActivity(intent);
+            }
+        });
+    }
+}
